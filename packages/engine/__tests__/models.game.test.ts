2039ba397983721647dada9bdd15ac346179c6d6
Michael Martin <mlmartin13@gmail.com>
2026-01-03 18:54:37 -0700

    Add indentation to game bank tests

--- packages/engine/__tests__/models.game.test.ts
+++ packages/engine/__tests__/models.game.test.ts
@@ -3,7 +3,6 @@ import { describe, expect, it } from 'vitest';
 import { size } from '../src/models/Bank';
 import {
   addPieceToBank,
-  GameSetupState,
   initial,
   switchActivePlayer,
   takePieceFromBank,
@@ -11,7 +10,7 @@ import {
 
 describe('Game', () => {
   it('should create an initial game state', () => {
-    const game: GameSetupState = initial();
+    const game = initial();
     expect(size(game.bank)).toBe(36);
     expect(game.activePlayer).toBe('player1');
     expect(game.homeSystems.player1.stars.length).toBe(0);
@@ -19,7 +18,7 @@ describe('Game', () => {
   });
 
   it('should switch the active player', () => {
-    const game: GameSetupState = initial();
+    const game = initial();
     expect(game.activePlayer).toBe('player1');
     expect(switchActivePlayer(game).activePlayer).toBe('player2');
     expect(switchActivePlayer(switchActivePlayer(game)).activePlayer).toBe(
@@ -27,38 +26,40 @@ describe('Game', () => {
     );
   });
 
-  it('should take a piece from the bank', () => {
-    const game: GameSetupState = initial();
-    const [piece, newGame] = takePieceFromBank(1, 'yellow', game);
-    expect(piece).toBeDefined();
-    expect(piece?.color).toBe('yellow');
-    expect(piece?.size).toBe(1);
-    expect(piece?.id).toBe('yellow-1-0');
-    expect(size(newGame.bank)).toBe(35);
-  });
+  describe('Bank', () => {
+    it('should take a piece from the bank', () => {
+      const game = initial();
+      const [piece, newGame] = takePieceFromBank(1, 'yellow', game);
+      expect(piece).toBeDefined();
+      expect(piece?.color).toBe('yellow');
+      expect(piece?.size).toBe(1);
+      expect(piece?.id).toBe('yellow-1-0');
+      expect(size(newGame.bank)).toBe(35);
+    });
 
-  it('should not take a piece from the bank if it is not available', () => {
-    const game1: GameSetupState = initial();
-    const [piece1, game2] = takePieceFromBank(1, 'yellow', game1);
-    const [piece2, game3] = takePieceFromBank(1, 'yellow', game2);
-    const [piece3, game4] = takePieceFromBank(1, 'yellow', game3);
-    const [piece4, game5] = takePieceFromBank(1, 'yellow', game4);
+    it('should not take a piece from the bank if it is not available', () => {
+      const game1 = initial();
+      const [piece1, game2] = takePieceFromBank(1, 'yellow', game1);
+      const [piece2, game3] = takePieceFromBank(1, 'yellow', game2);
+      const [piece3, game4] = takePieceFromBank(1, 'yellow', game3);
+      const [piece4, game5] = takePieceFromBank(1, 'yellow', game4);
 
-    expect(piece1).toBeDefined();
-    expect(piece2).toBeDefined();
-    expect(piece3).toBeDefined();
-    expect(piece4).toBeUndefined();
-    expect(size(game5.bank)).toBe(33); // 3 removed pieces, one attempt that did nothing
-  });
+      expect(piece1).toBeDefined();
+      expect(piece2).toBeDefined();
+      expect(piece3).toBeDefined();
+      expect(piece4).toBeUndefined();
+      expect(size(game5.bank)).toBe(33); // 3 removed pieces, one attempt that did nothing
+    });
 
-  it('should add a piece to the bank', () => {
-    const game1: GameSetupState = initial();
-    const [piece1, game2] = takePieceFromBank(1, 'yellow', game1);
+    it('should add a piece to the bank', () => {
+      const game1 = initial();
+      const [piece1, game2] = takePieceFromBank(1, 'yellow', game1);
 
-    expect(piece1).toBeDefined();
-    expect(size(game2.bank)).toBe(35);
+      expect(piece1).toBeDefined();
+      expect(size(game2.bank)).toBe(35);
 
-    const game3 = addPieceToBank(piece1!, game2);
-    expect(size(game3.bank)).toBe(36);
+      const game3 = addPieceToBank(piece1!, game2);
+      expect(size(game3.bank)).toBe(36);
+    });
   });
 });
