6a621206dc6965d113222cda530fd066f1050b16
Michael Martin <mlmartin13@gmail.com>
2026-01-16 19:16:54 -0500

    Simplify star system logic and fix exports

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -7,7 +7,9 @@
 //   const bank = Bank.addPiece(piece, Bank.empty);
 //   type MyBank = Bank.Bank;
 //   type MyPiece = GamePiece.Piece;
+import * as GameActionModule from './actions/GameAction';
 import * as BankModule from './models/Bank';
+import * as GameModule from './models/Game';
 import * as GamePieceModule from './models/GamePiece';
 import * as PlayerModule from './models/Player';
 import * as StarSystemModule from './models/StarSystem';
@@ -15,6 +17,8 @@ import * as StarSystemModule from './models/StarSystem';
 // Export namespaces - provides clear context for all exports
 export {
   BankModule as Bank,
+  GameModule as Game,
+  GameActionModule as GameAction,
   GamePieceModule as GamePiece,
   PlayerModule as Player,
   StarSystemModule as StarSystem,
